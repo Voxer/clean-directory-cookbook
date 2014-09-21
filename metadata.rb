ff51b44160edae82e7980cf26e58ae61ca0b0a4b
Anand Suresh <anandsuresh@gmail.com>
2014-09-20 20:13:21 -0700

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'infrastructure@voxer.com'
 license          'MIT License'
 description      'Remove stray (non-chef created) files from a directory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.0'
+version          '0.1.0'
