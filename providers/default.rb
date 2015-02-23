727ff3ab27742f529989ca799151c144b5035c9f
Dave Eddy <dave@daveeddy.com>
2015-02-23 12:59:01 -0500

    style, comments

--- providers/default.rb
+++ providers/default.rb
@@ -9,24 +9,25 @@
 
 use_inline_resources
 
-
 def whyrun_supported?
   true
 end
-
 
 action :clean do
   path = new_resource.path || new_resource.name
   raise 'clean_directory path must be a non-empty string!' if path.nil? or path.empty?
 
+  # find the chef files generated
   chef_files_generated = node.run_context.resource_collection.select { |r|
     new_resource.types.include?(r.class) and r.path.start_with?(path)
   }.map { |r| r.path }
 
+  # find the fileststem files present
   filesystem_files_present = Dir["#{path}/**/*"].select { |f|
     ::File.file?(f) or ::File.symlink?(f)
   }
 
+  # calculate the difference, and remove the extraneous files
   (filesystem_files_present - chef_files_generated).each do |f|
     if ::File.symlink?(f)
       link "clean_directory: #{f}" do
