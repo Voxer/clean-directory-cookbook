44655bfe30a2b46b2bc1e63e470bfa0b09d51961
Dave Eddy <dave@daveeddy.com>
2015-02-24 16:13:01 -0500

    use library instead of LWRP, fixes #2

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'clean-directory'
 maintainer       'Voxer'
-maintainer_email 'infrastructure@voxer.com'
+maintainer_email 'operations@voxer.com'
 license          'MIT License'
 description      'Remove stray (non-chef created) files from a directory'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
